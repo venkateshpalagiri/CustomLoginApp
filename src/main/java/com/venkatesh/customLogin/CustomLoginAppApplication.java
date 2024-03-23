fa6c31bf0f015da01fa29ec821a0ccc4a273cc8c
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-03-23 10:36:16 -0400

    init setup

---
+++ src/main/java/com/venkatesh/customLogin/CustomLoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.venkatesh.customLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLoginAppApplication.class, args);
+	}
+
+}
